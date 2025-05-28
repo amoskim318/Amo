90db550cbdae9d2cc2c5acd7c915886e349135b0
amoskim318 <amosk1430@gmail.com>
2025-05-28 07:58:43 +0300

    Create game.js

---
+++ game.js
@@ -0,0 +1,46 @@
+function myFunction(){
+document.getElementById("demo").innerHTMl=("hwlljhffgggddhjsjdhsjdgfhfhdudhdufhdgdodhfydhryjggg");
+  }
+    function myFunction (){
+    document.getElementById("demo").innerHTML=("<p>Nmap</p>Metasploit<p>Airclack-Ng</p>Wireshark<p></p>open VAS<p>SQLmap</p>NetStumber<p>EttetCap</p><p>maltego</p><p>nikto</p><p>Burp suit</p><p>John The Ripper</p><p>Angry Ip Scanner</p><p>Acunetix</p><p>Invicti(formerly NetSpark)</p><p>intruder</p><p>Remcos conclusion</p>");
+  }
+      function myFuncti(){
+      document.getElementById("mom").innerHTML=("<d2>HACKING TOOLS</d2><p>hacking</p>");
+    }
+  function myFunctio (){
+    document.getElementById("sit").innerHTML=("<p>UNLIMITED SSH</p><p>did.ecitizen.go.ke</p><header>CAPPED HOST</header><p>ncpwd.fuzu.com</p><p>betika.com</p><p>coregateway.app.dlight.com <header>AIRTEL</header><p>viton.com</p><p>104.18.8.127</p></p>");
+  }
+  function kenya1(){
+      document.getElementById("test1").innerHTML=("<p>Test Management Tools</p><p>Cross Browser Testing Tools</p><p>Best ETL Testing Tools</p><p>Mobile Testing Tools</p><p>Regression Testing Tools</p><p>Defect Tracking Tools</p><p>GUI Testing Tools</p><p>Requirements Management Tools</p>");
+    }
+    function kenya2(){
+      document.getElementById("test2").innerHTML=("<p>Identity card 30 USDT per Id</p><p>visa card</p>");
+    }
+        function kenya3(){
+      document.getElementById("test3").innerHTML=("<p>NORD VPN</p>");
+    }
+    function kenya4(){
+      document.getElementById("test4").innerHTML=("TFT TOOLS <P>MdmFix tools</P><p>Pandora</p><p>XTM ADB Tool v1.2 </p><p>MRT Tool V6.0  With 1 Year Free</p><p>XTM ADB TOOL V1.1  www.firmwarebd.com </p><p> FRP MTK </p><p>XTM MRT 3.95</p>");
+    }
+    function kenya5(){
+      document.getElementById("test5").innerHTML=("<p>CHAT ZONE</p>");
+    }
+    const toggleButton = document.getElementById('darkModeToggle');
+    toggleButton.addEventListener('click', () => {
+      document.body.classList.toggle('dark-mode');
+    });
+    function enya6() {
+      document.getElementById("tst6").innerHTML=("hello their");
+    }
+    function kenya6() {
+      document.getElementById("test6").innerHTML=("ghhhvvvh");
+    }
+    function toggleDrawer() {
+      const drawer = document.getElementById('drawer');
+      const facebook = document.getElementById('facebook');
+      drawer.classList.toggle('open');
+      facebook.classList.toggle('active');
+    }
+        function selectServer(fruit) {
+    document.getElementById("selectedServer").innerText = "" + fruit;
+}
